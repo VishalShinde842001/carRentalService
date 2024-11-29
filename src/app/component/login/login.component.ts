d2e368fcd7fd1389c29c12a7a8e2667c6bcd5ca6
sanket <sanketj24.rts@gmail.com>
2024-11-29 21:23:33 +0530

    login page

---
+++ src/app/component/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { DropdownModule } from 'primeng/dropdown';
+import { CommonService } from '../../service/common.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [DropdownModule, CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private commonService: CommonService) {
+
+  }
+  ngOnInit(): void {
+    this.getRoles();
+  }
+  loginData: any = {};
+  roleList: any = [];
+  showPassword: boolean = false;
+  toggleShowPassword() {
+    this.showPassword = !this.showPassword; 
+  }
+  getRoles() {
+    this.commonService.getRequest(this.commonService.SERVER_URL['ROLE_LIST']).subscribe((response: any) => {
+      if (response.status == 200) {
+        this.roleList = response.result
+      }
+    }, (error: any) => {
+
+    })
+  }
+
+  login() {
+    this.commonService.postRequest(this.commonService.SERVER_URL['LOGIN'], this.loginData).subscribe()
+  }
+
+}
