928e078cd9af1ada38f9453eb58a51db459e2bf7
sanket <sanketj24.rts@gmail.com>
2024-12-01 14:18:42 +0530

    added table to show car list on admin panel

--- src/app/component/admin/carlist/carlist.component.ts
+++ src/app/component/admin/carlist/carlist.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SideBarComponent } from '../side-bar/side-bar.component';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-carlist',
   standalone: true,
-  imports: [SideBarComponent],
+  imports: [SideBarComponent, CommonModule],
   templateUrl: './carlist.component.html',
   styleUrl: './carlist.component.css'
 })
-export class CarlistComponent {
+export class CarlistComponent implements OnInit {
+  ngOnInit(): void {
+    this.getCarList();
+  }
+
+  carList: any = [];
+  getCarList() {
+
+  }
 
 }
